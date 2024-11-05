4c0cab27d28e7764838e9f6163c17366dea32c98
mashi123 <22511909+mashi123@users.noreply.github.com>
2024-11-05 22:52:39 +0900

    work commit

--- work/schedule.py
+++ work/schedule.py
@@ -1,17 +1,19 @@
-from concurrent.futures import thread
-from zoneinfo import ZoneInfo
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
 import time
 
 
-def test():
+def func():
     print(f"{str(datetime.now())}: OK")
 
 
-scheduler = BackgroundScheduler(timezone=ZoneInfo("Asia/Tokyo"))
+# scheduler = BackgroundScheduler(timezone=ZoneInfo("Asia/Tokyo"))
 # scheduler = BackgroundScheduler(timezone=ZoneInfo("UTC"))
-scheduler.add_job(test, 'cron', hour='23', minute='*/1')
+scheduler = BackgroundScheduler()
+# scheduler.add_job(func, 'cron', hour='23', minute='*/1')
+scheduler.add_job(func, CronTrigger.from_crontab(
+    '50 22 * * *', timezone='Asia/Tokyo'))
 scheduler.start()
 
 time.sleep(10000)
